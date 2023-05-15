63e4ab754e65c795a546d8eea9b44001cc4c2c68
elifKurtay <elif.kurtay00@gmail.com>
2023-05-15 11:24:30 +0200

    fixed code

--- Assets/Scripts/Basket/ResetTransformation.cs
+++ Assets/Scripts/Basket/ResetTransformation.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class ResetTransformation : MonoBehaviour
 {
+    public Transform spawner;
+
+    private Rigidbody rigidBody;
+
+    public void Awake() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     public void reset() {
-        transform.position = new Vector3(0.083f-0.208f, 1f + 0.506f, 1.12f-0.378f);
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+        transform.position = spawner.position;
     }
 
 }
