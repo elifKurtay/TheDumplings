a4f4335eeb99793d01be27fc32f2b305c2ca3c96
fruspa <25087275+fruspa@users.noreply.github.com>
2023-05-16 23:18:29 +0200

    Fix basketball loop, add door disabling mechanism (to debug)

---
+++ Assets/Scripts/Door/DisableDoor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reset door position and disables it when the player enters the trigger
+public class DisableDoor : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject handler;
+    // public Collider trigger;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    // whether the door is disabled
+    private bool disabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform t = door.GetComponent<Transform>();
+        initialPosition = t.position;
+        initialRotation = t.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // when the player enters the trigger
+    private void OnTriggerEnter(Collider someObject) {
+        // already disabled
+        if (disabled) return;
+
+        ResetAndDisableDoor();
+        disabled = true;
+    }
+
+    private void ResetAndDisableDoor() {
+        // disable rigidbody
+        Rigidbody rb = door.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        door.transform.position = initialPosition;
+        door.transform.rotation = initialRotation;
+        
+
+        // reset door position
+        // rb.MovePosition(initialPosition);
+        // rb.MoveRotation(initialRotation);
+
+        // rb.detectCollisions = false;
+
+        // disable grabbable handler
+        handler.SetActive(false);
+    }
+
+}
