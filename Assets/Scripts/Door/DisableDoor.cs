cd572800ba2d8f58a477f48ec84d4b9f84a7be07
elifKurtay <elif.kurtay00@gmail.com>
2023-05-20 18:26:11 +0200

    door dis/en-able

--- Assets/Scripts/Door/DisableDoor.cs
+++ Assets/Scripts/Door/DisableDoor.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DisableDoor : MonoBehaviour
 {
     public GameObject door;
+    public DisableDoor otherDoor;
     public GameObject handler;
     public GameHandler gameHandler;
     // public Collider trigger;
@@ -31,34 +32,43 @@ public class DisableDoor : MonoBehaviour
     }
 
     // when the player enters the trigger
-    private void OnTriggerEnter(Collider someObject) {
+    private void OnTriggerExit(Collider someObject) {
         // already disabled
         if (disabled) return;
-
-        ResetAndDisableDoor();
-        if(gameHandler != null) {
-            gameHandler.NextLevel();
+        if (someObject.gameObject.layer == 8)
+        {
+            Disable();
+            if(gameHandler != null) {
+                gameHandler.NextLevel();
+            }
         }
-        disabled = true;
     }
 
-    private void ResetAndDisableDoor() {
+    private void Disable() {
         // disable rigidbody
         Rigidbody rb = door.GetComponent<Rigidbody>();
         rb.isKinematic = true;
 
         door.transform.position = initialPosition;
         door.transform.rotation = initialRotation;
-        
-
-        // reset door position
-        // rb.MovePosition(initialPosition);
-        // rb.MoveRotation(initialRotation);
-
-        // rb.detectCollisions = false;
-
         // disable grabbable handler
         handler.SetActive(false);
+        otherDoor.Enable();
+        disabled = true;
+    }
+    
+    public void Enable() {
+        Rigidbody rb = door.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        door.transform.position = initialPosition;
+        door.transform.rotation = initialRotation;
+
+        // disable grabbable handler
+        handler.SetActive(true);
+        disabled = false;
     }
 
 }
